b1111cadffc7c156bee94be282d71545206c2607
YeniRa31 <100779614+YeniRa31@users.noreply.github.com>
2022-04-22 20:06:31 +0700

    Add files via upload

---
+++ uts_Looping.py
@@ -0,0 +1,13 @@
+# Nama : Yeni Rahmawati
+# NIM : 191011400115
+# kelas : 06TPLM002
+
+# looping menggunakan While
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+print("\n")
+# Looping menggunakan for
+for x in range(6):
+    print(x)
